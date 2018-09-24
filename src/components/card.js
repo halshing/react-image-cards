7d060225bb734bcceadf52a82b62798f785bbd21
Hasan <hasan.alshingieti@gmail.com>
2018-09-24 11:05:36 -0400

    updates

--- src/components/card.js
+++ src/components/card.js
@@ -3,46 +3,34 @@ import { render } from "react-dom";
 
 const Card = ({ config }) => {
   let { card, size, margin, openModal } = config;
-  return (
-    <div
-      className="card-container"
-      onClick={() => {
+  return <div className="card-container" onClick={() => {
         openModal(card);
-      }}
-      data-image-url={card.Url}
-      data-image-id={
-        card.Url.substring(
-          card.Url.lastIndexOf("/") + 1,
-          card.Url.length
-        ).split(".")[0]
-      }
-    >
+      }} data-image-url={card.Url} data-image-id={card.Url.substring(card.Url.lastIndexOf("/") + 1, card.Url.length).split(".")[0]}>
       <div className="card-image">
         <img src={card.Url} title={card.Title} />
       </div>
       <div className="card-details">
         <div className="card-title">{card.Title.substring(0, 60)}</div>
         <div className="card-stats">
-          <div className="icon-container">
-            <div className="icon icon-upvote" />
+          <div className="icon-container icon-upvote">
+            <div className="icon" />
             <div className="stat-number">{card.Stats.Upvotes}</div>
           </div>
-          <div className="icon-container">
-            <div className="icon icon-downvote" />
+          <div className="icon-container icon-downvote">
+            <div className="icon" />
             <div className="stat-number">{card.Stats.Downvotes}</div>
           </div>
-          <div className="icon-container">
-            <div className="icon icon-comments" />
+        <div className="icon-container icon-comments">
+            <div className="icon" />
             <div className="stat-number">{card.Stats.Comments}</div>
           </div>
-          <div className="icon-container">
-            <div className="icon icon-views" />
+        <div className="icon-container icon-views">
+            <div className="icon" />
             <div className="stat-number">{card.Stats.Views}</div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    </div>;
 };
 
 export default Card;
