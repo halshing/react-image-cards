db1a20688fa8ac937ea3c5a4b4441b248c03d285
Hasan <hasan.alshingieti@gmail.com>
2018-09-27 22:35:47 -0400

    updates

--- src/components/cards.js
+++ src/components/cards.js
@@ -1,22 +1,15 @@
 import React, { Component } from "react";
-import { render } from "react-dom";
 import Card from "./card";
 
-class Cards extends Component {
-  state = {};
-
-  render() {
-    const { cards } = this.props;
-    //const { size, cols, margin } = this.state;
-
-    return (
-      <div className="all-cards">
-        {cards.map((card, index) => (
-          <Card key={index} config={{ card }} />
-        ))}
-      </div>
-    );
-  }
-}
+const Cards = props => {
+  const { cards } = props;
+  return (
+    <div className="all-cards">
+      {cards.map((card, index) => (
+        <Card key={index} config={{ card }} />
+      ))}
+    </div>
+  );
+};
 
 export default Cards;
