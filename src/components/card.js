bde15a4d9792c53bf272eed95d510b14f69461ec
Hasan <hasan.alshingieti@gmail.com>
2018-09-30 14:13:29 -0400

    updates

--- src/components/card.js
+++ src/components/card.js
@@ -3,28 +3,30 @@ import React, { Component } from "react";
 const Card = props => {
   let { card } = props.config;
   return (
-    <div className="col-xs-12 card-container" onClick={props.openImage}>
-      <div className="card-image">
-        <img src={card.link} title={card.title} />
-      </div>
-      <div className="card-details">
-        <div className="card-title">{card.title.substring(0, 60)}</div>
-        <div className="card-stats">
-          <div className="icon-container icon-upvote">
-            <div className="icon" />
-            <div className="stat-number">{card.ups}</div>
-          </div>
-          <div className="icon-container icon-downvote">
-            <div className="icon" />
-            <div className="stat-number">{card.downs}</div>
-          </div>
-          <div className="icon-container icon-comments">
-            <div className="icon" />
-            <div className="stat-number">{card.comment_count}</div>
-          </div>
-          <div className="icon-container icon-views">
-            <div className="icon" />
-            <div className="stat-number">{card.views}</div>
+    <div className="col-xs-12" onClick={props.openImage}>
+      <div className="card-container">
+        <div className="card-image">
+          <img src={card.link} title={card.title} />
+        </div>
+        <div className="card-details">
+          <div className="card-title">{card.title}</div>
+          <div className="card-stats">
+            <div className="icon-container icon-upvote">
+              <div className="icon" />
+              <div className="stat-number">{card.ups}</div>
+            </div>
+            <div className="icon-container icon-downvote">
+              <div className="icon" />
+              <div className="stat-number">{card.downs}</div>
+            </div>
+            <div className="icon-container icon-comments">
+              <div className="icon" />
+              <div className="stat-number">{card.comment_count}</div>
+            </div>
+            <div className="icon-container icon-views">
+              <div className="icon" />
+              <div className="stat-number">{card.views}</div>
+            </div>
           </div>
         </div>
       </div>
